ac3c6ee96ab08bc36e77ca8d6795348512a27111
Tatger Corentin <corentin.tatger@celadodc-rswl.com>
2022-03-16 16:24:12 +0100

    Notebook et markov commenté

--- markov.py
+++ markov.py
@@ -1,6 +1,5 @@
 # %%
 import matplotlib.pyplot as plt
-from math import gamma
 import seaborn as sns
 import numpy as np
 import pandas as pd
@@ -25,31 +24,32 @@ def evaluate_mk(df, mk):
 
 class MK_chain:
     def __init__(self, df):
-        """__init__ Markov chain Class constructor
+        """MK_chain class constructor, computes transition matrix and gamma from given dataframe
 
         Arguments:
-            Prob_mat  -- Transition matrix of the markov chain
-
-        Raises:
-            ValueError: Sum of probibilities in a given row should always add up to 1
+            df {pandas.dataframe} -- dataframe, containing clusters ID for start and arrival
         """
+        # Stored df as class data for ease of use
         self.data_frame = df
 
-        self.trips_nb = []
-        self.Start_clusters = []
-        self.End_clusters = []
+        # List used to store occurences of trip scenario. Used for proba computation
         self.sum_mat = []
+        # Used to check if proba in matrix add up to 1, margin for float approx
         self.TOLERANCE_VALUE = 0.0001
 
+        # Trip clusters informations
         self.Start_clusters = self.data_frame['gps_start_cluster'].unique()
         self.Start_clusters.sort()
         self.End_clusters = self.data_frame['gps_end_cluster'].unique()
         self.End_clusters.sort()
 
+        # Computes and stores Transition matrix and gamma vector
         self.create_transition_matrix()
 
     def create_sum_matrix(self):
-
+        """Parses dataset to store occurences of each start-end cluster combination
+           Format : [Start, End, nb_occurence]
+        """
         for Start in self.Start_clusters:
             Total = 0
             Starting_points = self.data_frame.loc[self.data_frame['gps_start_cluster'] == Start]
@@ -61,21 +61,40 @@ class MK_chain:
                 self.sum_mat.append([Start, End, Total])
 
     def create_proba_vector(self, Start):
+        """Creates an array containing probability for each existing end cluster, given a start cluster
 
+        Arguments:
+            Start {int} -- Start cluster for which to compute proba vector
+
+        Returns:
+            np.array -- Probability vector
+        """
         size = max(self.End_clusters[-1], self.Start_clusters[-1])
-        if size == 1:
+
+        # Avoid crashing in case first trip is from cluster 0 to cluster 0
+        if size == 0:
             size = 1
 
         proba_vector = np.zeros(size+1)
         starting_points = self.data_frame.loc[self.data_frame['gps_start_cluster'] == Start]
         sample = len(starting_points)
 
+        # Browse through sum_matrix (format [start_cluster, end_cluster, nb_occurence])
         for freq in self.sum_mat:
             if freq[0] == Start:
+                # If start_cluster is equal to Start, computes probability
                 proba_vector[freq[1]] = (freq[2]/sample)*100
         return proba_vector
 
     def create_gamma(self):
+        """Computes gamma vector, can be used for first prediction at the start
+
+        Raises:
+            ValueError: Probability in gamma have to add up to 1 (with a tolerated margin)
+
+        Returns:
+            list -- gamma vector, chances of each start cluster to be selected as starting point of trip
+        """
         gamma = []
         sample = len(self.data_frame)
 
@@ -87,10 +106,16 @@ class MK_chain:
         return gamma
 
     def create_transition_matrix(self):
+        """Computes Markov chain's transition matrix, of dimension (n*n) with n the nb of clusters.
+           Each line is computed using the "create_proba_vector" method
 
+        Returns:
+            np.array -- Transition matrix of MK_chain instance
+        """
         self.create_sum_matrix()
 
         size = max(self.End_clusters[-1], self.Start_clusters[-1])
+        # Avoid crashing in case first trip is from cluster 0 to cluster 0
         if size == 0:
             size = 1
         t_mat = np.array(self.create_proba_vector(0))
@@ -104,16 +129,21 @@ class MK_chain:
 
         self.states = [str(x) for x in range(len(self.transitionMatrix))]
 
-        return gamma, t_mat
+        return self.gamma, t_mat
 
     def fit(self, new):
+        """Updates transition matrix and gamma vector when new trip is given. Adds the trip to the existing database
 
+        Arguments:
+            new {pandas.dataframe} --Single entry dataframe representing one trip
+        """
         start = new['gps_start_cluster'].values
         end = new['gps_end_cluster'].values
 
         frames = [self.data_frame, new]
         self.data_frame = pd.concat(frames)
 
+        # If new line contains cluster id not existing previously, computing gamma and T_mat all over again is needed
         if (start > self.Start_clusters[-1] or end > self.End_clusters[-1]):
             self.Start_clusters = self.data_frame['gps_start_cluster'].unique()
             self.Start_clusters.sort()
@@ -142,13 +172,21 @@ class MK_chain:
                 self.gamma = self.create_gamma()
 
     def predict(self, curr_st):
+        """Gives id of predicted end cluster, given start of trip
 
+        Arguments:
+            curr_st {str} -- Id of current trip starting cluster
+
+        Returns:
+            str -- Predicted end cluster for trip
+        """
         if curr_st in self.states:
             st_id = self.states.index(curr_st)
             most_likely = max(self.transitionMatrix[st_id])
             pred_id = np.where(self.transitionMatrix[st_id] == most_likely)[0][0]
             return self.states[pred_id]
         else:
+            # If given cluster Id not existing in states, outputs -1
             return('-1')
 
 
